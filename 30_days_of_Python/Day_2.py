4b47d3f76ffa54188a2c8389d6b85cb69d38cd9d
Your Name <your.email@example.com>
2025-10-19 23:46:21 +0300

    I learned about variables and datatypes

---
+++ 30_days_of_Python/Day_2.py
@@ -0,0 +1,14 @@
+num = 30
+print("The number is an integer:", num)
+
+name = "Francis"
+print("This is a string :",name)
+
+is_student = True
+print("This is a bool:", is_student)
+
+height = 5.9
+print("This is a float:", height)
+
+print("My name is", name,", I am", num, "years old.Its", is_student, "that I am a student and my height is", height, "feet.")
+
